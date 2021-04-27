1042d5c25514229ee85c33320b4c638f1397b338
Nimisha Ranipa <nimisha.ranipa@bacancy.com>
2021-04-27 17:50:36 +0530

    Local Video Strech issue solved

--- ios/Classes/Base/RtcSurfaceView.swift
+++ ios/Classes/Base/RtcSurfaceView.swift
@@ -82,7 +82,7 @@ class RtcSurfaceView: UIView {
             surface = EffectPlayerView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height / 2)))
             //surface.layoutIfNeeded()
             banubaSdkManager.setup(configuration: EffectPlayerConfiguration(renderMode: .video))
-            banubaSdkManager.setRenderTarget(layer: surface.layer as! CAEAGLLayer, contentMode : RenderContentMode.resize, playerConfiguration: nil)
+            banubaSdkManager.setRenderTarget(layer: surface.layer as! CAEAGLLayer, contentMode : RenderContentMode.resizeAspectFill, playerConfiguration: nil)
         }
         else {
             surface = UIView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height / 2)))

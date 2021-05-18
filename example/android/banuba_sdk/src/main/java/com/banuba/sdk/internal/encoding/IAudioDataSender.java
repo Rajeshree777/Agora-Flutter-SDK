be0ec3c229727117957b22bdd2ebdf4f4a561d2c
Nimisha <nimisha.ranipa@bacancy.com>
2021-05-18 19:55:55 +0530

    unneccesory folder removed

--- example/android/banuba_sdk/src/main/java/com/banuba/sdk/internal/encoding/IAudioDataSender.java
+++
@@ -1,6 +0,0 @@
-package com.banuba.sdk.internal.encoding;
-
-
-import androidx.annotation.NonNull;
-
-public interface IAudioDataSender { void sendAudioData(@NonNull byte[] buffer, long audioTimeNs); }
